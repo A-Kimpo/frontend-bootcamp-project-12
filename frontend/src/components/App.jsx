5fdfae8df05d801f94502042156ead1e2353df78
A-Kimpo <ak.kimpo@gmail.com>
2023-05-20 18:44:16 +0500

    Created context and logic for moving through components
    Updated App.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,13 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useLocation,
+} from 'react-router-dom';
 
-function App() {
+import {
+  Button,
+  Navbar,
+  Nav,
+  Container,
+} from 'react-bootstrap';
+
+import LoginPage from './LoginPage';
+import SignUpPage from './SignUpPage';
+import Page404 from './Page404';
+import MainPage from './chat/MainPage';
+import AuthContext from '../contexts/index';
+import useAuth from '../hooks/index';
+
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello</p>
-      </header>
-    </div>
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
   );
-}
+};
+
+const PrivateRoute = ({ children }) => {
+  const auth = useAuth();
+  const location = useLocation();
+
+  return (
+    auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+  );
+};
+
+const AuthButton = () => {
+  const auth = useAuth();
+
+  return (
+    auth.loggedIn
+      ? <Button onClick={auth.logOut}>Log out</Button>
+      : null
+  );
+};
+
+const App = () => (
+  <AuthProvider>
+    <Router>
+      <div className="d-flex flex-column h-100">
+        <Navbar expand="lg" bg="white" className="shadow-sm">
+          <Container>
+            <Navbar.Brand as={Link} to="/">Slack chat</Navbar.Brand>
+            <Nav className="mr-auto" />
+            <AuthButton />
+          </Container>
+        </Navbar>
+
+        <Routes>
+          <Route path="*" element={<Page404 />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/"
+            element={(
+              <PrivateRoute>
+                <MainPage />
+              </PrivateRoute>
+            )}
+          />
+        </Routes>
+      </div>
+    </Router>
+  </AuthProvider>
+);
 
 export default App;
