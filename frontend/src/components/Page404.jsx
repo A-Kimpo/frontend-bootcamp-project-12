8ae5e2cf1cbe66d814f48282a414ec329adc0d20
A-Kimpo <ak.kimpo@gmail.com>
2023-05-21 19:24:05 +0500

    Update Page404.jsx

--- frontend/src/components/Page404.jsx
+++ frontend/src/components/Page404.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Page404 = () => (
-  <div className="container h-100 my-4 overflow-hidden rounded shadow">
-    Not Found
+  <div className="text-center">
+    <img src="page404.png" className="img-fluid h-25" alt="Страница не найдена" />
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p className="text-muted">
+      Но вы можете перейти
+      &nbsp;
+      <a href="/">на главную страницу</a>
+    </p>
   </div>
 );
 
