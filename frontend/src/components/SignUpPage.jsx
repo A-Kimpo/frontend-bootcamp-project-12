d7bafc9ebdb2be9b2de4a16913a04038bf3f6fc1
A-Kimpo <ak.kimpo@gmail.com>
2023-05-21 19:23:44 +0500

    Rewrited SignUpPage.jsx by react-bootstrap

--- frontend/src/components/SignUpPage.jsx
+++ frontend/src/components/SignUpPage.jsx
@@ -1,63 +1,72 @@
 import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Form,
+  Button,
+  Image,
+} from 'react-bootstrap';
 
 const SignUpPage = () => (
-  <div class="container-fluid h-100">
-    <div class="row justify-content-center align-content-center h-100">
-      <div class="col-12 col-md-8 col-xxl-6">
-        <div class="card shadow-sm">
-          <div class="card-body d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
+  <Container fluid className="h-100">
+    <Row className="justify-content-center align-content-center h-100">
+      <Col xs={12} md={8} xxl={6}>
+        <Card className="shadow-sm">
+          <Card.Body className="d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
             <div>
-              <img src="signUpImage.jpg" class="rounded-circle" alt="Регистрация" />
+              <Image src="signUpImage.jpg" roundedCircle alt="Регистрация" />
             </div>
-            <form class="w-50">
-              <h1 class="text-center mb-4">Регистрация</h1>
-              <div class="form-floating mb-3">
-                <input
-                  placeholder="От 3 до 20 символов"
-                  name="username"
-                  autoComplete="username"
-                  autoFocus
-                  required
+            <Form className="w-50">
+              <h1 className="text-center mb-4">Регистрация</h1>
+              <Form.Floating className="mb-3">
+                <Form.Control
                   id="username"
-                  class="form-control"
-                />
-                <label class="form-label" htmlFor="username">Имя пользователя</label>
-                <div placement="right" class="invalid-tooltip">Обязательное поле</div>
-              </div>
-              <div class="form-floating mb-3">
-                <input
-                  placeholder="Не менее 6 символов"
-                  name="password"
-                  aria-describedby="passwordHelpBlock"
+                  name="username"
                   required
-                  autoComplete="new-password"
-                  type="password"
+                  autoFocus
+                  placeholder="От 3 до 20 символов"
+                  autoComplete="username"
+                  className="form-control"
+                />
+                <label className="form-label" htmlFor="username">Имя пользователя</label>
+                <Form.Control.Feedback type="invalid" placement="right" tooltip>Обязательное поле</Form.Control.Feedback>
+              </Form.Floating>
+              <Form.Floating className="mb-3">
+                <Form.Control
                   id="password"
-                  class="form-control"
-                />
-                <div class="invalid-tooltip">Обязательное поле</div>
-                <label class="form-label" htmlFor="password">Пароль</label>
-              </div>
-              <div class="form-floating mb-4">
-                <input
-                  placeholder="Пароли должны совпадать"
-                  name="confirmPassword"
-                  required
-                  autoComplete="new-password"
+                  name="password"
                   type="password"
-                  id="confirmPassword"
-                  class="form-control"
+                  required
+                  placeholder="Не менее 6 символов"
+                  autoComplete="new-password"
+                  aria-describedby="passwordHelpBlock"
+                  className="form-control"
                 />
-                <div class="invalid-tooltip" />
-                <label class="form-label" htmlFor="confirmPassword">Подтвердите пароль</label>
-              </div>
-              <button type="submit" class="w-100 btn btn-outline-primary">Зарегистрироваться</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+                <label className="form-label" htmlFor="password">Пароль</label>
+                <Form.Control.Feedback type="invalid" tooltip>Обязательное поле</Form.Control.Feedback>
+              </Form.Floating>
+              <Form.Floating className="mb-4">
+                <Form.Control
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  required
+                  placeholder="Пароли должны совпадать"
+                  autoComplete="new-password"
+                  className="form-control"
+                />
+                <label className="form-label" htmlFor="confirmPassword">Подтвердите пароль</label>
+                <Form.Control.Feedback type="invalid" tooltip />
+              </Form.Floating>
+              <Button type="submit" variant="outline-primary" className="w-100">Зарегистрироваться</Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default SignUpPage;
