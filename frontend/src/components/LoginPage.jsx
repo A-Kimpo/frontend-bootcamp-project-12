da9c3b602c66cb37533f7a59738cdc2a2275ebba
A-Kimpo <ak.kimpo@gmail.com>
2023-05-21 19:21:50 +0500

    Rewrited LoginPage.jsx by react-bootstrap

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -1,63 +1,63 @@
 import React from 'react';
-// import { Button, Form } from 'react-bootstrap';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Form,
+  Button,
+  Image,
+} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => (
-  <div className="container-fluid h-100">
-    <div className="row justify-content-center align-content-center h-100">
-      <div className="col-12 col-md-8 col-xxl-6">
-        <div className="card shadow-sm">
-          <div className="card-body row p-5">
-            <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-              <img
-                src="loginImage.jpeg"
-                className="rounded-circle"
-                alt="Войти"
-              />
-            </div>
-            <form className="col-12 col-md-6 mt-3 mt-mb-0">
+  <Container fluid className="h-100">
+    <Row className="justify-content-center align-content-center h-100">
+      <Col xs={12} md={8} xxl={6}>
+        <Card className="shadow-sm">
+          <Card.Body className="row p-5">
+            <Col xs={12} md={6} className="d-flex align-items-center justify-content-center">
+              <Image src="loginImage.jpeg" roundedCircle alt="Войти" />
+            </Col>
+            <Form className="col-12 col-md-6 mt-3 mt-mb-0">
               <h1 className="text-center mb-4">Войти</h1>
-              <div className="form-floating mb-3">
-                <input
-                  name="username"
-                  autoСomplete="username"
-                  autoFocus
-                  required
-                  placeholder="Ваш ник"
+              <Form.Floating className="mb-3">
+                <Form.Control
                   id="username"
-                  className="form-control"
+                  name="username"
+                  autoFocus
+                  placeholder="Ваш ник"
+                  autoComplete="username"
                 />
-                <label htmlFor="username">
-                  Ваш ник
-                </label>
-              </div>
-              <div className="form-floating mb-4">
-                <input
-                  name="password"
-                  autoСomplete="current-password"
-                  required
-                  placeholder="Пароль"
-                  type="password"
+                <label htmlFor="username">Ваш ник</label>
+              </Form.Floating>
+              <Form.Floating className="mb-4">
+                <Form.Control
                   id="password"
-                  className="form-control"
+                  name="password"
+                  type="password"
+                  placeholder="Пароль"
+                  autoComplete="current-password"
                 />
-                <label className="form-label" htmlFor="password">
-                  Пароль
-                </label>
-              </div>
-              <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
-            </form>
-          </div>
-          <div className="card-footer p-4">
+                <label htmlFor="password">Пароль</label>
+                <Form.Control.Feedback type="invalid">
+                  the username or password is incorrect
+                </Form.Control.Feedback>
+              </Form.Floating>
+              <Button type="submit" variant="outline-primary" className="w-100 mb-3">Войти</Button>
+            </Form>
+          </Card.Body>
+          <Card.Footer className="p-4">
             <div className="text-center">
               <span>Нет аккаунта?</span>
               &nbsp;
-              <a href="/signup">Регистрация</a>
+              <Link to="/signup">Регистрация</Link>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+          </Card.Footer>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default LoginPage;
