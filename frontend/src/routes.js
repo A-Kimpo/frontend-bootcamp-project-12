59e0fe263399a6d3b88be481a6a2477ddba3aa50
A-Kimpo <ak.kimpo@gmail.com>
2023-05-24 14:55:32 +0500

    Connect redux/redux-toolkit, update routes.js

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,5 +2,5 @@ const apiPath = '/api/v1';
 
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
-  usersPath: () => [apiPath, 'data'].join('/'),
+  dataPath: () => [apiPath, 'data'].join('/'),
 };
