a92afebd3128aa18b1f89a5af8c65e81f47b0603
A-Kimpo <ak.kimpo@gmail.com>
2023-05-28 13:10:54 +0500

    Implement removing messages of channel

--- frontend/src/slices/messagesSlice.js
+++ frontend/src/slices/messagesSlice.js
@@ -1,5 +1,7 @@
 import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
 
+import { actions as channelsActions } from './channelsSlice';
+
 const messagesAdapter = createEntityAdapter();
 const initialState = messagesAdapter.getInitialState();
 
@@ -10,6 +12,12 @@ const messagesSlice = createSlice({
     addMessage: messagesAdapter.addOne,
     addMessages: messagesAdapter.addMany,
   },
+  extraReducers: (builder) => builder
+    .addCase(channelsActions.removeChannel, (state, { payload: id }) => {
+      const updatedMessages = Object.values(state.entities)
+        .filter(({ channelId }) => id !== channelId);
+      messagesAdapter.setMany(state, updatedMessages);
+    }),
 });
 
 export const { actions } = messagesSlice;
