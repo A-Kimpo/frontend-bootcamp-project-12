a1646997c3b6e26a564aee44b6907df1f8e9164f
A-Kimpo <ak.kimpo@gmail.com>
2023-05-29 08:25:27 +0500

    Update App.jsx

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,50 +4,74 @@ import {
   Routes,
   Route,
   Link,
+  Navigate,
+  useLocation,
 } from 'react-router-dom';
 import {
   Navbar,
+  Button,
   Nav,
   Container,
 } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
+import MainPage from './chat/MainPage';
 import LoginPage from './LoginPage';
 import SignUpPage from './SignUpPage';
 import Page404 from './Page404';
-import MainPage from './chat/MainPage';
-import AuthProvider, { PrivateRoute, AuthButton } from '../providers/AuthProvider';
-import SocketProvider from '../providers/SocketProvider.jsx';
+import { useAuth } from '../providers/AuthProvider';
 
-const App = () => (
-  <AuthProvider>
-    <SocketProvider>
-      <Router>
-        <div className="d-flex flex-column h-100">
-          <Navbar expand="lg" bg="white" className="shadow-sm">
-            <Container>
-              <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
-              <Nav className="mr-auto" />
-              <AuthButton />
-            </Container>
-          </Navbar>
+const AuthButton = ({ t }) => {
+  const { loggedIn, logOut } = useAuth();
 
-          <Routes>
-            <Route path="*" element={<Page404 />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route
-              path="/"
-              element={(
-                <PrivateRoute>
-                  <MainPage />
-                </PrivateRoute>
-              )}
-            />
-          </Routes>
-        </div>
-      </Router>
-    </SocketProvider>
-  </AuthProvider>
-);
+  return (
+    loggedIn
+      ? <Button onClick={logOut}>{t('logOutButton')}</Button>
+      : null
+  );
+};
+
+const PrivateRoute = ({ children }) => {
+  const { loggedIn } = useAuth();
+  const location = useLocation();
+
+  return (
+    loggedIn
+      ? children
+      : <Navigate to="/login" state={{ from: location }} />
+  );
+};
+
+const App = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Router>
+      <div className="d-flex flex-column h-100">
+        <Navbar expand="lg" bg="white" className="shadow-sm">
+          <Container>
+            <Navbar.Brand as={Link} to="/">{t('appName')}</Navbar.Brand>
+            <Nav className="mr-auto" />
+            <AuthButton t={t} />
+          </Container>
+        </Navbar>
+
+        <Routes>
+          <Route path="*" element={<Page404 t={t} />} />
+          <Route path="/signup" element={<SignUpPage t={t} />} />
+          <Route path="/login" element={<LoginPage t={t} />} />
+          <Route
+            path="/"
+            element={(
+              <PrivateRoute>
+                <MainPage />
+              </PrivateRoute>
+            )}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
