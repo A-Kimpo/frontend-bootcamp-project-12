e34de02cea3f6b6f3cfdae80aa32f5447a394300
A-Kimpo <ak.kimpo@gmail.com>
2023-06-01 14:26:29 +0500

    Fix Rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -3,17 +3,17 @@ import { Provider as StoreProvider } from 'react-redux';
 import { initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
 import { io } from 'socket.io-client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import leoProfanity from 'leo-profanity';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 import store from './slices';
 import resources from './locales';
-import App from './components/App';
 import AuthProvider from './providers/AuthProvider';
 import SocketProvider from './providers/SocketProvider';
+import App from './components/App';
 
 export default async () => {
   await i18n.use(initReactI18next)
@@ -22,13 +22,13 @@ export default async () => {
       resources,
     });
 
+  const socket = io();
+
   leoProfanity.loadDictionary('ru');
   leoProfanity.loadDictionary('en');
 
-  const socket = io();
-
   const rollbarConfig = {
-    accessToken: 'ed45a4bbc50d4b43a0ae593918e8c63d',
+    accessToken: process.env.ROLLBAR_TOKEN,
     environment: 'testenv',
   };
 
